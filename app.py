b0a267f2892db2b8aa4f6cbf9d0bf699d05bf7fc
richyy1-code <richboampong20@gmail.com>
2025-05-15 02:47:46 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
@@ -15,4 +16,5 @@ def home():
     return render_template("index.html", now=now, birthday=birthday)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
